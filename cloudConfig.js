728380cfb4efcefd410c19eeea49b03d5a7385a9
Sandeep Kumar <s.k.student1035@gmail.com>
2024-10-06 12:19:20 +0530

    Update Changes

--- cloudConfig.js
+++ cloudConfig.js
@@ -4,18 +4,18 @@ const { CloudinaryStorage } = require('multer-storage-cloudinary');
 cloudinary.config({
     cloud_name: process.env.CLOUD_NAME,
     api_key: process.env.CLOUD_API_KEY,
-    api_secret: process.env.Cloud_API_SECRET,
+    api_secret: process.env.CLOUD_API_SECRET,
 });
 
 const storage = new CloudinaryStorage({
     cloudinary: cloudinary,
     params: {
       folder: 'wanderlust_DEV',
-     allowerdFormats:["png","jpg","jpeg"],
+      allowed_formats: ['jpeg', 'png', 'jpg']
     },
   });
 
    module.exports = {
     cloudinary,
-    storage,
-   }
+    storage
+   };
